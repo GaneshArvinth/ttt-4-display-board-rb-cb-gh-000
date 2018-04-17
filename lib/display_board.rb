fa5fc36a24f2391ee1ddd2ec0b2b21cc8f7ae039
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-17 04:51:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (elements)
+  puts (" #{elements[0]} | #{elements[1]} | #{elements[2]} ")
+  puts ("-----------")
+  puts (" #{elements[3]} | #{elements[4]} | #{elements[5]} ")
+  puts ("-----------")
+  puts (" #{elements[6]} | #{elements[7]} | #{elements[8]} ")
+end
